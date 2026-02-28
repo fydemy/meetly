cbf3d79a8b0df125eed264aa9aefa104e1ffe3bd
devswithme <developswithme@gmail.com>
2026-02-28 12:36:08 +0800

    fix: build error

---
+++ src/types/editorjs-modules.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Type declarations for Editor.js tools that do not ship with their own types
+ * or whose package.json "exports" do not expose typings to the resolver.
+ */
+declare module "@editorjs/link";
+declare module "@editorjs/embed";
